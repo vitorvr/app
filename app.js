0a681f4a5bb85d4fa293d7eda193a11a5cffc735
Vitor <vitorviannaribeiro@gmail.com>
2017-05-19 00:19:46 -0300

    alteracoes

--- app.js
+++ app.js
@@ -5,16 +5,26 @@ const cors = require('cors');
 const passport = require('passport');
 const mongoose = require('mongoose');
 
+
+//Using expressjs
 const app = express();
+
+
+//Port of server
 const port = 3000;
 
-app.get('/', (req, res) => {
-	res.send('Invalid Path');
+//Using CORS to allow differents domains
+app.use(cors());
+
+//Body Parser
+app.use(bodyParser.json());
+
+//Index
+app.get('/', (request, response) => {
+	response.send('Invalid Path');
 });
 
-
-
-
+//Satar server
 app.listen(port, () => {
 	console.log("Server started on port " + port);
 });
