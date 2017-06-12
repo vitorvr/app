fcba2f8a2ba9b6062414261126e93a62db5f1a9a
Ribeiro, Vitor Vianna <vitor.vianna.ribeiro@accenture.com>
2017-06-12 16:02:03 -0300

    Angular2, Navegação, Login, Logout

---
+++ angular-src/src/app/components/register/register.component.ts
@@ -0,0 +1,57 @@
+import { Component, OnInit } from '@angular/core';
+import { ValidateService } from '../../services/validate.service';
+import { AuthService } from '../../services/auth.service';
+import { FlashMessagesService } from 'angular2-flash-messages';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.css']
+})
+export class RegisterComponent implements OnInit {
+  name: String;
+  username: String;
+  email: String;
+  password: String;
+
+  constructor(private validateService: ValidateService, 
+              private flashMessages: FlashMessagesService,
+              private authService: AuthService,
+              private router: Router) { }
+
+  ngOnInit() {
+  }
+
+  onRegisterSubmit(){
+    const user = {
+      name: this.name,
+      username: this.username,
+      email: this.email,
+      password: this.password
+    }
+
+    if(!this.validateService.validateRegister(user)){
+      this.flashMessages.show('Please fill in all fields', {cssClass: 'alert-danger', timeout: 3000});
+      return false;
+    }
+
+    if(!this.validateService.validateEmail(user.email)){
+      this.flashMessages.show('Please use a valid email', {cssClass: 'alert-danger', timeout: 3000});
+      return false;
+    }
+
+    //Register User
+    this.authService.registerUser(user).subscribe(data => {
+      if(data.success){
+        this.flashMessages.show('You are now registered and can log in', {cssClass: 'alert-success', timeout: 3000});
+        this.router.navigate(['/login']);
+      }else{
+        this.flashMessages.show('Something went wrong', {cssClass: 'alert-success', timeout: 3000});
+        this.router.navigate(['/register']);
+      }
+    });
+
+  }
+
+}
